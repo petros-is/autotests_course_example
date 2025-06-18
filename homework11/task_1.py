7a7aa67fd17cdecbe5806b482c3287d3f04ddbe6
petros-is <60099406+petros-is@users.noreply.github.com>
2025-06-18 08:11:52 +0300

    Update task_1.py

--- homework11/task_1.py
+++ homework11/task_1.py
@@ -11,18 +11,27 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-sbis_site = 'https://sbis.ru/'
+sbis_site = 'https://saby.ru/'
 driver = webdriver.Chrome()
 try:
     driver.get(sbis_site)
+    assert driver.current_url == 'https://saby.ru/'
     contact_btn = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu')
     contact_btn.click()
+    sleep(1)
+    contact_menu = driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__items')
+    assert contact_menu.is_displayed()
     other_btn = driver.find_element(By.CSS_SELECTOR, "[href='/contacts']")
     other_btn.click()
+    contact_url = 'https://saby.ru/contacts/'
+    sleep(1)
+    assert contact_url in driver.current_url
     logo_btn = driver.find_element(By.CSS_SELECTOR, '.sbisru-Contacts__logo-tensor')
     logo_btn.click()
 
+
     driver.switch_to.window(driver.window_handles[1])
+    assert driver.current_url == 'https://tensor.ru/'
     pwr = driver.find_element(By.CSS_SELECTOR, '.tensor_ru-Index__block4-content')
     pwr.location_once_scrolled_into_view
     assert pwr.is_displayed()
